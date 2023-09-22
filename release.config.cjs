c2ad0ec2f4920e99455700ff1e6500041b73641b
Gavin Mogan <git@gavinmogan.com>
2023-09-21 22:01:07 -0700

    fix: have release notes

--- release.config.cjs
+++ release.config.cjs
@@ -34,6 +34,12 @@ module.exports = {
     ],
     [
       "@semantic-release/release-notes-generator",
+      {
+        "preset": "angular",
+        "parserOpts": {
+          "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        }
+      }
     ],
     [
       "@semantic-release/changelog",
